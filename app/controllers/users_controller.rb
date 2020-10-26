68bc833e4d08a8baa299c8f859146c179d76eec0
hvduan97 <hvduan97@gmail.com>
2020-10-26 13:32:11 +0700

    fix chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :load_user, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page]).per_page(Settings.page)
+  end
+
   def new
     @user = User.new
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-
-    flash[:warning] = t "controllers.users_controller.user_not_found"
-    redirect_to root_path
-  end
+  def show; end
 
   def create
     @user = User.new user_params
@@ -22,10 +25,54 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "controllers.users_controller.profile_update"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "controllers.users_controller.user_delete"
+    else
+      flash[:danger] = t "controllers.users_controller.delete_fail"
+    end
+    redirect_to users_path
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "controllers.users_controller.please_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t "controllers.users_controller.user_not_found"
+    redirect_to root_path
   end
 end
